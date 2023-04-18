c870d58601428a2b913c53f66051e8803e430eda
IhabMks <ihab.ozark@gmail.com>
2023-04-18 18:50:21 +0200

    Link2+Collision

--- src/zelda/Zelda.java
+++ src/zelda/Zelda.java
@@ -1,8 +1,6 @@
 package zelda;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 
 import zelda.scenary.Quest;
@@ -13,32 +11,61 @@ import com.golden.gamedev.Game;
 import com.golden.gamedev.GameLoader;
 
 public class Zelda extends Game {
-    
+
     private Link link;
-   
+    private Link link1;
+
     private Quest quest;
-    
+
     private boolean menu;
-    
+
     public Zelda() {
-        
+
     }
+
     private Rock rock;
 
     public void initResources() {
 
         this.quest = new Quest(this);
         this.link = new Link(this);
-        this.link.setBoard(this.quest.getCurrentBoard());    
+        this.link.setBoard(this.quest.getCurrentBoard());
         this.menu = false;
+        this.link1 = new Link(this);
+        this.link1.setBoard(this.quest.getCurrentBoard());
+        this.link1.setLocation(200, 200); //544
+        this.link1.setSpeed(0, 0);
         System.out.println("width: " + this.getWidth() + " height: " + this.getHeight());
     }
-        
+
     public void update(long elapsedTime) {
         boolean left = false;
         boolean right = false;
         boolean up = false;
         boolean down = false;
+        boolean canMove = true;
+        Rectangle linkRect = new Rectangle(
+                (int) this.link.getX(), (int) this.link.getY(),
+                link.getWidth(), link.getHeight()
+        );
+
+        Rectangle link1Rect = new Rectangle(
+                (int) this.link1.getX(), (int) this.link1.getY(),
+                link.getWidth(), link.getHeight()
+        );
+
+        if (linkRect.intersects(link1Rect)) {
+            canMove = false;
+            if (link.getOrientation() == Orientation.WEST) {
+                this.link.setLocation(this.link1.getX() + this.link1.getWidth() + 5, this.link.getY());
+            } else if (link.getOrientation() == Orientation.EAST) {
+                this.link.setLocation(this.link1.getX() - this.link.getWidth() - 5, this.link.getY());
+            } else if (link.getOrientation() == Orientation.NORTH) {
+                this.link.setLocation(this.link.getX(), this.link1.getY() + this.link1.getHeight() + 5);
+            } else if (link.getOrientation() == Orientation.SOUTH) {
+                this.link.setLocation(this.link.getX(), this.link1.getY() - this.link.getHeight() - 5);
+            }
+        }
 
         if (this.keyPressed(KeyEvent.VK_ALT)) {
             this.link.fight();
@@ -55,28 +82,28 @@ public class Zelda extends Game {
         } else {
             this.link.setSpeed(0, 0);
         }
-        System.out.println(this.link.getX() +" " + this.link.getY());
+        System.out.println(this.link.getX() + " " + this.link.getY());
         //System.out.println("width: " + this.getWidth() + " height: " + this.getHeight());
         if (link.getOrientation() == Orientation.WEST) {
-            if (link.getX() < - 10) {
+            if (link.getX() < -10) {
                 left = true;
             }
         }
         if (link.getOrientation() == Orientation.EAST) {
 
-            if(this.link.getX() > this.getHeight() +50){
+            if (this.link.getX() > this.getHeight() + 50) {
                 right = true;
             }
         }
         if (link.getOrientation() == Orientation.SOUTH) {
 
-            if(this.link.getY() > 543){
+            if (this.link.getY() > 543) {
                 down = true;
             }
         }
         if (link.getOrientation() == Orientation.NORTH) {
 
-            if(this.link.getY() < 115){
+            if (this.link.getY() < 115) {
                 up = true;
             }
         }
@@ -100,8 +127,10 @@ public class Zelda extends Game {
             this.link.setLocation(this.link.getX(), 115); //115 ==> hauteur du fond noir
             this.link.setAnimationFrame(0, 0);
         }
+
         this.quest.update(elapsedTime);
         this.link.update(elapsedTime);
+        this.link1.update(elapsedTime);
     }
 
     public void render(Graphics2D g) {
@@ -109,12 +138,13 @@ public class Zelda extends Game {
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
         this.quest.render(g);
         this.link.render(g);
+        this.link1.render(g);
     }
-    
+
     public static void main(String[] args) {
         GameLoader game = new GameLoader();
-        game.setup(new Zelda(), new Dimension(672,588), false);
+        game.setup(new Zelda(), new Dimension(672, 588), false);
         game.start();
     }
-    
+
 }
--- src/zelda/scenary/Floor.java
+++ src/zelda/scenary/Floor.java
@@ -6,25 +6,32 @@ import com.golden.gamedev.object.Sprite;
 import zelda.Zelda;
 
 public class Floor extends AbstractTile {
-    
+
     public enum Color {
         GREEN,
-        DARCK,
+        DARCK1,
+        DARCK2,
         SAND
     }
+
     private Color color;
-    
+
     public Floor(Zelda game, Color color) {
         super(game, 1, 1, 42, 42);
         this.color = color;
-        switch(color) {
-        case SAND:
-            this.add(new Sprite(this.game.getImage("res/sprites/scenary/BGGF.gif")), -1);
-            break;
-        case GREEN:
-            break;
-        case DARCK:
-            break;
+        switch (color) {
+            case SAND:
+                this.add(new Sprite(this.game.getImage("res/sprites/scenary/BGGF.gif")), -1);
+                break;
+            case GREEN:
+                this.add(new Sprite(this.game.getImage("res/sprites/scenary/BGGB.gif")), -1);
+                break;
+            case DARCK1:
+                this.add(new Sprite(this.game.getImage("res/sprites/Dongeon/1/DG1F7-G.gif")), -1);
+                break;
+            case DARCK2:
+                this.add(new Sprite(this.game.getImage("res/sprites/Dongeon/4/DG4F6.gif")), -1);
+                break;
         }
     }
 }
